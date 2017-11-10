d99dcd89c06421cae31be124b9b1f43d56d290c5
tomo426.sakura1281m@gmail.com <tomo426.sakura1281m@gmail.com>
2017-11-10 11:35:59 +0900

    go 4.hs but miss

--- warmUp/4.hs
+++ warmUp/4.hs
@@ -1,4 +1,20 @@
+-- 
+import Data.List
+
+getAlp :: String -> String
+getAlp = filter (`elem` ['A'..'z'] ++ [' '])
+
+getNum :: [Int] -> [Int]
+getNum = filter (:)
+
+getElements :: [Int] -> [String] -> [String]
+getElements [] _          = []
+getElements _  []         = []
+getElements (n:ns) (s:ss) = (take n s) : (getElements ns ss)
+
 main = do
         let str = "Hi He Lied Beacuse Boron Could Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
-        let array = [1, 5, 6, 7, 8, 9, 15, 16, 19]
-        get
+        let ary = [1, 5, 6, 7, 8, 9, 15, 16, 19]
+        let wrd = words $ getAlp str 
+        print $ getElements ary wrd
+        
